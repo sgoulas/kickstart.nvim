44502ab0b623509b9b5042a98ba06e776aada9b3
sgoulas <sprgoulas@gmail.com>
2026-01-29 00:06:49 +0200

    add treesitter context to comment plugin for differentiating between js and jsx

--- lua/custom/plugins/comment.lua
+++ lua/custom/plugins/comment.lua
@@ -1,18 +1,32 @@
--- Comment.nvim - Toggle comments
+-- Comment.nvim - Toggle comments with JSX/TSX support
 return {
     'numToStr/Comment.nvim',
     event = 'VeryLazy',
-    opts = {
-        padding = true,
-        sticky = true,
-        ignore = nil,
-        toggler = {
-            line = 'gcc',
-            block = 'gbc',
-        },
-        opleader = {
-            line = 'gc',
-            block = 'gb',
-        },
+    dependencies = {
+        'JoosepAlviste/nvim-ts-context-commentstring',
     },
+    config = function()
+        -- Skip backwards compat routines and speed up loading
+        vim.g.skip_ts_context_commentstring_module = true
+
+        require('ts_context_commentstring').setup({
+            enable_autocmd = false,
+        })
+
+        require('Comment').setup({
+            padding = true,
+            sticky = true,
+            ignore = nil,
+            toggler = {
+                line = 'gcc',
+                block = 'gbc',
+            },
+            opleader = {
+                line = 'gc',
+                block = 'gb',
+            },
+            -- Use ts_context_commentstring to determine comment style
+            pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+        })
+    end,
 }
