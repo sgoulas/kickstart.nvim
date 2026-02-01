e7abd7045e003181504a4a700884a190d43ed4fd
sgoulas <sprgoulas@gmail.com>
2026-02-01 02:23:17 +0200

    update neo tree configuration to follow current file and keep directories open

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,38 +1,44 @@
--- Neo-tree file explorer
+-- Neo-tree filve explorer
 return {
-    'nvim-neo-tree/neo-tree.nvim',
-    version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons',
-        'MunifTanjim/nui.nvim',
-    },
-    lazy = false,
-    keys = {
-        { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-    },
-    opts = {
-        filesystem = {
-            filtered_items = {
-                visible = true,
-            },
-            window = {
-                mappings = {
-                    ['\\'] = 'close_window',
-                },
-            },
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,
         },
         window = {
-            mappings = {
-                ['<C-f>'] = 'none',
-                ['<C-b>'] = 'none',
-                ['<C-j>'] = { 'scroll_preview', config = { direction = -10 } },
-                ['<C-k>'] = { 'scroll_preview', config = { direction = 10 } },
-                ['s'] = 'none',
-                ['S'] = 'none',
-                ['x'] = 'open_split',
-                ['v'] = 'open_vsplit',
-            },
+          mappings = {
+            ['\\'] = 'close_window',
+          },
         },
-    },
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = true,
+        },
+      },
+      window = {
+        mappings = {
+          ['<C-f>'] = 'none',
+          ['<C-b>'] = 'none',
+          ['<C-j>'] = { 'scroll_preview', config = { direction = -10 } },
+          ['<C-k>'] = { 'scroll_preview', config = { direction = 10 } },
+          ['s'] = 'none',
+          ['S'] = 'none',
+          ['x'] = 'open_split',
+          ['v'] = 'open_vsplit',
+        },
+      },
+    }
+  end,
 }
