1599c416c0f50019aa713d0e65db30ec0aa0c6d7
sgoulas <sprgoulas@gmail.com>
2026-02-06 20:47:28 +0200

    make neo-tree cover half sceen when opened

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,44 +1,45 @@
 -- Neo-tree filve explorer
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
-    'MunifTanjim/nui.nvim',
-  },
-  lazy = false,
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  config = function()
-    require('neo-tree').setup {
-      filesystem = {
-        filtered_items = {
-          visible = true,
-        },
-        window = {
-          mappings = {
-            ['\\'] = 'close_window',
-          },
-        },
-        follow_current_file = {
-          enabled = true,
-          leave_dirs_open = true,
-        },
-      },
-      window = {
-        mappings = {
-          ['<C-f>'] = 'none',
-          ['<C-b>'] = 'none',
-          ['<C-j>'] = { 'scroll_preview', config = { direction = -10 } },
-          ['<C-k>'] = { 'scroll_preview', config = { direction = 10 } },
-          ['s'] = 'none',
-          ['S'] = 'none',
-          ['x'] = 'open_split',
-          ['v'] = 'open_vsplit',
-        },
-      },
-    }
-  end,
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons',
+        'MunifTanjim/nui.nvim',
+    },
+    lazy = false,
+    keys = {
+        { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    },
+    config = function()
+        require('neo-tree').setup {
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                },
+                window = {
+                    mappings = {
+                        ['\\'] = 'close_window',
+                    },
+                },
+                follow_current_file = {
+                    enabled = true,
+                    leave_dirs_open = true,
+                },
+            },
+            window = {
+                width = "50%",
+                mappings = {
+                    ['<C-f>'] = 'none',
+                    ['<C-b>'] = 'none',
+                    ['<C-j>'] = { 'scroll_preview', config = { direction = -10 } },
+                    ['<C-k>'] = { 'scroll_preview', config = { direction = 10 } },
+                    ['s'] = 'none',
+                    ['S'] = 'none',
+                    ['x'] = 'open_split',
+                    ['v'] = 'open_vsplit',
+                },
+            },
+        }
+    end,
 }
