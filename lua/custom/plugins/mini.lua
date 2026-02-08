d96dc7e93ca3c4aacfa25921358ab7bfa0d9750f
sgoulas <sprgoulas@gmail.com>
2026-02-08 23:34:12 +0200

    remove mini status line plugin

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -8,14 +8,5 @@ return {
         --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
         --  - ci'  - [C]hange [I]nside [']quote
         require('mini.ai').setup { n_lines = 500 }
-
-        -- Simple and easy statusline
-        local statusline = require 'mini.statusline'
-        statusline.setup { use_icons = vim.g.have_nerd_font }
-
-        ---@diagnostic disable-next-line: duplicate-set-field
-        statusline.section_location = function()
-            return '%2l:%-2v'
-        end
     end,
 }
