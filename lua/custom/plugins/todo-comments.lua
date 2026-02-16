068e45627102d46e7395a86f110d1feb86c79538
sgoulas <sprgoulas@gmail.com>
2026-02-17 00:29:26 +0200

    update treesitter to also include js, ts and tsx, add comments to todo comments plugin

--- lua/custom/plugins/todo-comments.lua
+++ lua/custom/plugins/todo-comments.lua
@@ -1,7 +1,14 @@
 -- Highlight todo, notes, etc in comments
+-- these keywords need to be followed by `:`
+-- PERF: optimized
+-- HACK: hacky
+-- TODO: update in the future
+-- NOTE: keep in mind
+-- FIX: this should be fixed
+-- WARNING: beware
 return {
-    'folke/todo-comments.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
+  'folke/todo-comments.nvim',
+  event = 'VimEnter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = { signs = false },
 }
--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,27 +1,30 @@
 -- Treesitter - Syntax highlighting and code parsing
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
-    opts = {
-        ensure_installed = {
-            'bash',
-            'c',
-            'diff',
-            'html',
-            'lua',
-            'luadoc',
-            'markdown',
-            'markdown_inline',
-            'query',
-            'vim',
-            'vimdoc',
-        },
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { 'ruby' },
-        },
-        indent = { enable = true, disable = { 'ruby' } },
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  main = 'nvim-treesitter.configs',
+  opts = {
+    ensure_installed = {
+      'bash',
+      'c',
+      'diff',
+      'html',
+      'javascript',
+      'lua',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'query',
+      'tsx',
+      'typescript',
+      'vim',
+      'vimdoc',
     },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { 'ruby' },
+    },
+    indent = { enable = true, disable = { 'ruby' } },
+  },
 }
