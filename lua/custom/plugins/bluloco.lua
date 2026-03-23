0bb10a6ed37edc1b3c612a6c35d85479c0b630e8
sgoulas <sprgoulas@gmail.com>
2026-03-23 17:57:28 +0200

    remove theme setting code from blu-loco theme pluging

--- lua/custom/plugins/bluloco.lua
+++ lua/custom/plugins/bluloco.lua
@@ -1,17 +1,16 @@
 -- Bluloco colorscheme (light)
 return {
-    'uloco/bluloco.nvim',
-    lazy = false,
-    priority = 1000,
-    dependencies = { 'rktjmp/lush.nvim' },
-    config = function()
-        require('bluloco').setup {
-            style = 'light',
-            transparent = false,
-            italics = false,
-            terminal = vim.fn.has('gui_running') == 1,
-            guicursor = true,
-        }
-        vim.cmd.colorscheme 'bluloco'
-    end,
+  'uloco/bluloco.nvim',
+  lazy = false,
+  priority = 1000,
+  dependencies = { 'rktjmp/lush.nvim' },
+  config = function()
+    require('bluloco').setup {
+      style = 'light',
+      transparent = false,
+      italics = false,
+      terminal = vim.fn.has 'gui_running' == 1,
+      guicursor = true,
+    }
+  end,
 }
