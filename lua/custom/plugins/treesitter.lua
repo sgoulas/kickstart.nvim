77bf725674171ffd095bbfb1a70c964289c385f6
sgoulas <sprgoulas@gmail.com>
2026-05-12 00:56:07 +0300

    yet another attemp at fixing tree-sitter
    can't go around installing tree-sitter cli it seems

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,8 +1,34 @@
--- Treesitter - Neovim 0.12 has native treesitter support
--- Highlighting and indent are enabled by default
--- Keeping plugin only for parser installation via :TSInstall / :TSUpdate
+-- Treesitter - Neovim 0.12 migration
+-- Requires the tree-sitter CLI (brew install tree-sitter-cli)
 return {
     'nvim-treesitter/nvim-treesitter',
-    branch = 'master',
+    branch = 'main',
     build = ':TSUpdate',
+    main = 'nvim-treesitter',
+    init = function()
+        -- Enable treesitter highlighting and indentation for every filetype
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                pcall(vim.treesitter.start)
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+
+        -- Auto-install parsers if missing
+        local ensure_installed = {
+            'lua', 'python', 'typescript', 'tsx', 'javascript',
+            'html', 'css', 'scss', 'json', 'yaml', 'toml',
+            'bash', 'markdown', 'markdown_inline', 'vim', 'vimdoc',
+            'regex', 'diff', 'gitcommit',
+        }
+        local installed = require('nvim-treesitter.config').get_installed()
+        local to_install = vim.iter(ensure_installed)
+            :filter(function(parser)
+                return not vim.tbl_contains(installed, parser)
+            end)
+            :totable()
+        if #to_install > 0 then
+            require('nvim-treesitter').install(to_install)
+        end
+    end,
 }
